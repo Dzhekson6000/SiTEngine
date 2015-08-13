e116e4a02b12cbdf686185b5150d420c9a2a1401
Dzhekson6000 <Dzhekson6000@yandex.ru>
2015-08-13 20:01:50 +0300

    Add ext macros

--- SiT/Macros.h
+++ SiT/Macros.h
@@ -40,3 +40,27 @@ void log(const char * format, ...);
 #define CALLBACK_1(__selector__,__target__, ...) std::bind(&__selector__,__target__, std::placeholders::_1, ##__VA_ARGS__)
 #define CALLBACK_2(__selector__,__target__, ...) std::bind(&__selector__,__target__, std::placeholders::_1, std::placeholders::_2, ##__VA_ARGS__)
 #define CALLBACK_3(__selector__,__target__, ...) std::bind(&__selector__,__target__, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, ##__VA_ARGS__)
+
+#define CREATE_FUNC(__TYPE__) \
+static __TYPE__* create()\
+{\
+	__TYPE__ *ret = new __TYPE__();\
+	if (ret && ret->init())\
+	{\
+		return (__TYPE__*)ret->autorelease();\
+	}\
+	else\
+	{\
+		delete ret;\
+		return nullptr;\
+	}\
+}
+
+#define SYNTHESIZE(varType, varName, funName)\
+protected: varType varName;\
+public: virtual varType get##funName(void) const { return varName; }\
+public: virtual void set##funName(varType var){ varName = var; }
+
+#define SYNTHESIZE_READONLY(varType, varName, funName)\
+protected: varType varName;\
+public: virtual varType get##funName(void) const { return varName; }
