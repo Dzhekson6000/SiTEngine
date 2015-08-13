ff0d36d01dea6c8ab3976421901e03a0c863fe67
Dzhekson6000 <Dzhekson6000@yandex.ru>
2015-08-13 20:06:46 +0300

    add color const

---
+++ SiT/Geometry/Color.cpp
@@ -0,0 +1,15 @@
+#include "Color.h"
+
+NS_SIT_BEGIN
+
+const Color Color::WHITE(255, 255, 255);
+const Color Color::YELLOW(255, 255, 0);
+const Color Color::GREEN(0, 255, 0);
+const Color Color::BLUE(0, 0, 255);
+const Color Color::RED(255, 0, 0);
+const Color Color::MAGENTA(255, 0, 255);
+const Color Color::BLACK(0, 0, 0);
+const Color Color::ORANGE(255, 127, 0);
+const Color Color::GRAY(166, 166, 166);
+
+NS_SIT_END
--- SiT/Geometry/Color.h
+++ SiT/Geometry/Color.h
@@ -1,14 +1,33 @@
 #ifndef COLOR_H_
 #define COLOR_H_
 
-struct Color
+#include "Macros.h"
+
+NS_SIT_BEGIN
+
+class Color
 {
-	float _r;
-	float _g;
-	float _b;
+private:
+	SYNTHESIZE(float, _r, R);
+	SYNTHESIZE(float, _g, G);
+	SYNTHESIZE(float, _b, B);
+public:
 
 	Color() :_r(0), _g(0), _b(0){}
 	Color(float r, float g, float b) :_r(r), _g(g), _b(b){}
+	Color(int r, int g, int b):_r(r/255),_g(g / 255),_b(b/255){}
+
+	const static Color WHITE;
+	const static Color YELLOW;
+	const static Color BLUE;
+	const static Color GREEN;
+	const static Color RED;
+	const static Color MAGENTA;
+	const static Color BLACK;
+	const static Color ORANGE;
+	const static Color GRAY;
 };
+
+NS_SIT_END
 
 #endif  // COLOR_H_
